951f52eb08980907d33a3d0c2ffa3dea3bfa2243
orelrahum <44848947+orelrahum@users.noreply.github.com>
2019-12-14 23:26:03 +0200

    git

--- src/ex1.java
+++ src/ex1.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class ex1 {
 
 	public static void main(String[] args) throws IOException {
-		readfile f=new readfile("input3.txt");
+		readfile f=new readfile("input4.txt");
 		Network Net=f.read();
 		Net.print();
 		//Net.makeoutput(Net.Answers);
