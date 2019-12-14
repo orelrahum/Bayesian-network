f40f8e2c4782f6886068d7759ffb79e66069c33d
orelrahum <44848947+orelrahum@users.noreply.github.com>
2019-12-14 15:59:39 +0200

    first VM

--- src/CPT.java
+++ src/CPT.java
@@ -6,26 +6,60 @@ class CTPParents{
 	public ArrayList<String> parents_names = new ArrayList<String>();
 	public ArrayList<String> parents_values = new ArrayList<String>();
 	//public Map<String, String> parents_value = new HashMap<>();
+	public CTPParents () {}
+	public CTPParents(CTPParents other) {
+
+		for (int i=0;i<this.parents_names.size();i++) {
+			other.parents_names.add(this.parents_names.get(i));
+		}
+		for (int i=0;i<this.parents_values.size();i++) {
+			other.parents_values.add(this.parents_values.get(i));
+		}
+	}
 }
+
+
 class CTPValues{
 	public ArrayList<String> value = new ArrayList<String>();
 	public ArrayList<Double> prob = new ArrayList<Double>();
 	//public Map<String, Double> value_prob = new HashMap<>();
+	public CTPValues() {}
+	public CTPValues(CTPValues other) {
+		for (int i=0;i<this.value.size();i++) {
+			other.value.add(this.value.get(i));
+		}
+		for (int i=0;i<this.prob.size();i++) {
+			other.prob.add(this.prob.get(i));
+		}	
+	}
 }
 
-public class CPT {	
+public class CPT {
+	String Name;
 	public ArrayList<CTPParents> parents_values = new ArrayList<CTPParents>();
 	public ArrayList<CTPValues> values_prob = new ArrayList<CTPValues>();
-	String Name;
-	String given_the;
-	
+
+
 	public CPT() {}
 
+	public CPT(CPT other) {
+		this.Name+=other.Name;
+		for (int i=0;i<this.parents_values.size();i++) {
+			CTPParents temp=new CTPParents(this.parents_values.get(i));
+			other.parents_values.add(temp);
+		}
+		for (int i=0;i<this.values_prob.size();i++) {
+			CTPValues temp=new CTPValues(this.values_prob.get(i));
+			other.values_prob.add(temp);	
+		}
+
+	}
+
 	public void print() {
-//		for(int i=0;i<this.values_prob.size();i++) {
-//			System.out.print(this.parents_values.get(i).parents_value);
-//			System.out.println(this.values_prob.get(i).value_prob);
-//		}
-	
+		//		for(int i=0;i<this.values_prob.size();i++) {
+		//			System.out.print(this.parents_values.get(i).parents_value);
+		//			System.out.println(this.values_prob.get(i).value_prob);
+		//		}
+
 	}
 }
--- src/VarElim.java
+++ src/VarElim.java
@@ -5,12 +5,40 @@ import java.util.Map;
 public class VarElim {
 
 	public static String VarElimAnswer(Network Net,String Query) {
+		ArrayList<String> given_the_name = new ArrayList<String>();
+		ArrayList<String> given_the_value = new ArrayList<String>();
+		String firstSplit[]=Query.split("\\|");
+		String Temp= new String (firstSplit[0]);
+		Temp=Temp.substring(2);
+		String secondSplit[]=Temp.split("=");
+		String Name=new String(secondSplit[0]);
+		String Value=new String (secondSplit[1]);
+		//		if (firstSplit.length>1) {
+		//			String Temp2=new String (firstSplit[1]);
+		//			String SecondAplit2[]=Temp2.split(")");
+		//			if (SecondAplit2.length>1) {
+		//				String Temp3=new String(SecondAplit2[0]);
+		//				String ThirdSplit3[]=Temp3.split(",");
+		//				for (int i=0;i<ThirdSplit3.length-1;i++) {
+		//					System.out.println(SecondAplit2[i]);
+		//					System.out.println(SecondAplit2[i+1]);
+		//					given_the_name.add(SecondAplit2[i]);
+		//					given_the_value.add(SecondAplit2[i+1]);
+		//				}
+		//			}
+		//		}
+
 		String Answer=new String("varelim");
+		ArrayList<CPT> CPT_vec = new ArrayList<CPT>();
+		for (int i=0;i<Net.Vars.size();i++) {
+			CPT temp= new CPT (Net.Vars.get(i).cpt);
+			CPT_vec.add(temp);
+		}
 		ArrayList<String> WhatToKill = new ArrayList<String>();
-		Map<String, String> Given_the = new HashMap<>();
-		String Name;
-		String Value;
-		
+
+
+
+
 		return Answer;
 	}
 }
